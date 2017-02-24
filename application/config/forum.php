1c8a7effcb0de5038b862b32b53b5ea49664e016
harpreetsnowflakes <harpreet.singh@snowflakessoftware.com>
2017-02-24 15:55:25 +0530

    Add Files

---
+++ application/config/forum.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Forum config
+|--------------------------------------------------------------------------
+|
+| This determines the basic forum config
+|
+*/
+$config['site_title'] = 'disussion';
+$config['site_slogan'] = 'disussion';
+$config['site_email'] = 'admin@gmail.com';
